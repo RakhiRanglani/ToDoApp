77864693b5498d7b9a40f15a162135d3fe61a713
Rakhi Ranglani <rakhi.ranglani222@outlook.com>
2021-10-10 17:00:01 +0100

    initial commit

---
+++ src/app/models/todo-item.model.ts
@@ -0,0 +1,8 @@
+export class TodoItem {
+    itemId: number=0;
+    itemName: string="";
+    itemDescription: string="";
+    itemStatus: boolean=false;
+    userName:string="";
+    lastUpdated:Date=new Date();
+}
